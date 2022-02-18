7433b9997fb252cf50b81df3b1549cd982dd2222
Zaid Baghal <4283944+zaidsbaghal@users.noreply.github.com>
2022-02-17 23:52:08 -0500

    update

---
+++ wordle+/pages/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="wordle">
+    <div class="header">
+      <h1 class="title">Wordle+</h1>
+    </div>
+    <div class="matrix">
+      {{ matrix }}
+    </div>
+    <div class="footer">
+      <button class="restart-btn">Restart</button>
+      <button class="new-word-btn">New Word</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      matrix: [
+        [1, 2, 3, 4, 5],
+        [1, 2, 3, 4, 5],
+        [1, 2, 3, 4, 5],
+        [1, 2, 3, 4, 5],
+        [1, 2, 3, 4, 5],
+        [1, 2, 3, 4, 5],
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.wordle {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  width: 100vw;
+  .header {
+    .title {
+      width: 100%;
+      display: flex;
+      justify-content: center;
+    }
+  }
+  .matrix {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .footer {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  }
+}
+</style>
